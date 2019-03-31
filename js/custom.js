b463840af57a87e43938d2c1919c3b206419029a
Ezekiel-H <Ezekiel.j.p.h@gmail.com>
2019-03-31 17:26:53 +0200

    Add cat pics

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,10 @@
+
 $('#carouselExample').on('slide.bs.carousel', function (e) {
 
-    /*
-
-    CC 2.0 License Iatek LLC 2018
-    Attribution required
-    
-    */
-
+  
     var $e = $(e.relatedTarget);
     var idx = $e.index();
-    var itemsPerSlide = 3;
+    var itemsPerSlide = 4;
     var totalItems = $('.carousel-item').length;
     
     if (idx >= totalItems-(itemsPerSlide-1)) {
@@ -25,3 +20,23 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
         }
     }
 });
+
+
+  $('#carouselExample').carousel({ 
+                interval: 2000
+        });
+
+
+  $(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function(event){
+      event.preventDefault();
+      var content = $('.modal-body');
+      content.empty();
+        var title = $(this).attr("title");
+        $('.modal-title').html(title);        
+        content.html($(this).html());
+        $(".modal-profile").modal({show:true});
+    });
+
+  });
